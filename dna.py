14cd4cba547f56ad35b640884f2488f42283a878
Daymortel <daymortel@gmail.com>
2021-10-14 13:44:46 +0200

    Update dna.py

--- dna.py
+++ dna.py
@@ -2,62 +2,62 @@ from sys import argv, exit
 import itertools
 import csv
 import re
-# check if there are the required amount of command line args.
+# vérifiez s'il y a la quantité requise d'arguments de ligne de commande.
 if len(argv) != 3:
     print(f"Error there should be 2 argv, you have {argv}")
     exit(1)
-# Open CSV and read to a list.
-with open(argv[1],"r") as inputfile:
+# Ouvrez CSV et lisez une liste.
+with open(argv[1], "r") as inputfile:
     reader = list(csv.reader(inputfile))
     reader[0].remove("name")
     i = reader[0]
-#Open Sequence TXT
-with open(argv[2],"r") as sequence:
+# Ouvrir la séquence TXT
+with open(argv[2], "r") as sequence:
     data = sequence.read()
-# i is a segement of DNA which contains the data from the CSV we are looking for.
-# for each sequence
+# i est un segment d'ADN qui contient les données du CSV que nous recherchons.
+# pour chaque séquence
 valuelist = []
-for q in range(len(i)): #eg. for the small CSV i = 3. so iterates for each nucleotide.
+for q in range(len(i)):  # par exemple. pour le petit CSV i3. donc itérer pour chaque nucléotide.
     maxcounter = 0
     counter = 0
     position = 0
     previouspos = 0
-    # while the dna sequence has not been fully scanned through do the following.
+    # alors que la séquence d'ADN n'a pas été entièrement analysée, procédez comme suit.
     while position < len(data):
-        # this gives the position at which the sequence is found
+        # cela donne la position à laquelle la séquence est trouvée
         position = data.find(i[q], position)
-        if position == -1: # i.e not found, reset the counter, stop the loop.
+        if position == -1:  # c'est-à-dire introuvable, réinitialiser le compteur, arrêter la boucle.
             counter = 0
             break
-        # if not -1 then the sequence being searchef for has been found and if (position - the length of the sequence) is also equal to 0, it is a consequtive value
-        #if sequence is at the start of the sequence
+        # sinon -1 alors la séquence recherchée a été trouvée et si (position - la longueur de la séquence) est également égale à 0, c'est une valeur consécutive
+        # si la séquence est au début de la séquence
         elif (position != -1) and previouspos == 0:
             counter += 1
             maxcounter = counter
             previouspos = position
-        #sequential occurances
+        # occurrences séquentielles
         elif (position != -1) and ((position - len(i[q])) == previouspos):
             counter += 1
             previouspos = position
             if maxcounter < counter:
                 maxcounter = counter
-        #first found and not at the start of the sequence.
+        # trouvé en premier et non au début de la séquence.
         elif (position != -1) and ((position - len(i[q])) != previouspos):
             counter = 1
             previouspos = position
             if maxcounter < counter:
                 maxcounter = counter
         position += 1
-    #record the largest number of sequencial occurances. 
+    # enregistrer le plus grand nombre d'occurrences séquentielles.
     valuelist.append(maxcounter)
 
-#the following compares the occurances of each nucliotide to the databases  
-#update the list to be a list of strings to enable comparison.
+# ce qui suit compare les occurrences de chaque nucléotide aux bases de données
+# mettre à jour la liste pour qu'elle soit une liste de chaînes pour permettre la comparaison.
 valuelist = list(map(str, valuelist))
-#make a new list to preserve reader
+# faire une nouvelle liste pour préserver le lecteur
 cleaned = list(reader)
 cleaned.pop(0)
-#compare valuelist to reader and if found print the name of the person whos DNA has all the occurances to the console/terminal.
+# comparez la liste de valeurs au lecteur et si trouvé, imprimez le nom de la personne dont l'ADN a toutes les occurrences sur la console/le terminal.
 for person in cleaned:
     if person[1:] == valuelist:
         print(f"{person[0]}")
